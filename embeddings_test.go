8b13f84e7cc8b6f5adae897a11790867704621bd
Ilya Kowalewski <badt@appar.at>
2024-11-29 21:34:36 +0200

    busthorne: tests to reflect recent changes

--- embeddings_test.go
+++ embeddings_test.go
@@ -41,7 +41,7 @@ func TestEmbedding(t *testing.T) {
 				"The food was delicious and the waiter",
 				"Other examples of embedding request",
 			},
-			Model: model,
+			Model: string(model),
 		}
 		// marshal embeddingReq to JSON and confirm that the model field equals
 		// the AdaSearchQuery type
@@ -57,7 +57,7 @@ func TestEmbedding(t *testing.T) {
 				"The food was delicious and the waiter",
 				"Other examples of embedding request",
 			},
-			Model: model,
+			Model: string(model),
 		}
 		marshaled, err = json.Marshal(embeddingReqStrings)
 		checks.NoError(t, err, "Could not marshal embedding request")
@@ -71,7 +71,7 @@ func TestEmbedding(t *testing.T) {
 				{464, 2057, 373, 12625, 290, 262, 46612},
 				{6395, 6096, 286, 11525, 12083, 2581},
 			},
-			Model: model,
+			Model: string(model),
 		}
 		marshaled, err = json.Marshal(embeddingReqTokens)
 		checks.NoError(t, err, "Could not marshal embedding request")
@@ -176,7 +176,7 @@ func TestAzureEmbeddingEndpoint(t *testing.T) {
 	)
 	// test create embeddings with strings (simple embedding request)
 	res, err := client.CreateEmbeddings(context.Background(), openai.EmbeddingRequest{
-		Model: openai.AdaEmbeddingV2,
+		Model: string(openai.AdaEmbeddingV2),
 	})
 	checks.NoError(t, err, "CreateEmbeddings error")
 	if !reflect.DeepEqual(res.Data, sampleEmbeddings) {
